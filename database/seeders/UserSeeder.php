e59fa27cad2effc7a1e73f4f6a9c0eead6ef3190
Andre Pollmeier <pollmeier.a@gmail.com>
2024-01-29 21:39:44 +0100

    seed users for admin and super-admin

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use GuzzleHttp\Promise\Create;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // create super-admin
+        $user = User::factory()->create([
+            'email' => 'super-admin@hofnaehe.de',
+            'password' => Hash::make('super-admin'),
+        ]);
+        $user->assignRole('Super Admin');
+
+        // create admin
+        $user = User::factory()->create([
+            'email' => 'admin@hofnaehe.de',
+            'password' => Hash::make('admin'),
+        ]);
+        $user->assignRole('Admin');
+    }
+}
